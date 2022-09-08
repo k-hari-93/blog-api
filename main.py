9167cd45832ba07113f8af67af91a108d874eb53
Hari Kizhedath <hari@mail.uni-paderborn.de>
2022-09-08 22:05:49 +0200

    create post, retrieve post by id

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+from gettext import find
 from typing import Optional
 from fastapi import FastAPI
-from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
+
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
 
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+my_posts = [
+    {
+        "id": 1,
+        "title": "Favourite Drinks", 
+        "content": "Tea and Coffee", 
+        "published": True, 
+        "rating": "4"
+    },
+    {
+        "id": 2,
+        "title": "Favourite Passtimes", 
+        "content": "Books and Music", 
+        "published": False, 
+        "rating": "5"
+    }
+]
 
 app = FastAPI()
 
@@ -17,9 +40,15 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "List of blog posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post.dict())
-    return "new post created"
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return { "data": post_dict }
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    return { "data": find_post(id) }
