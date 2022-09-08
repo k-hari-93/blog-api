7eae8dc8e87f06420f36df83314935df8e7f8f46
Hari Kizhedath <hari@mail.uni-paderborn.de>
2022-09-08 22:26:28 +0200

    correcting status codes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from gettext import find
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -25,7 +24,7 @@ my_posts = [
     },
     {
         "id": 2,
-        "title": "Favourite Passtimes", 
+        "title": "Favourite Pastimes", 
         "content": "Books and Music", 
         "published": False, 
         "rating": "5"
@@ -42,7 +41,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(0, 1000000)
@@ -50,5 +49,9 @@ def create_posts(post: Post):
     return { "data": post_dict }
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    return { "data": find_post(id) }
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"post with {id} was not found")
+    return { "data": post }
