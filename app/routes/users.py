a3016e381511afe6f470a48d14311c778914cae2
Hari Kizhedath <hari@mail.uni-paderborn.de>
2022-09-11 11:05:57 +0200

    add tags to paths

--- app/routes/users.py
+++ app/routes/users.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import Session
 from .. import models, schemas, utils
 from ..database import get_db
 
-router = APIRouter(prefix="/users")
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
