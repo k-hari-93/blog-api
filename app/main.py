c3db1113e0f18199ab4c312a096c1d4fcae00cac
Hari Kizhedath <hari@mail.uni-paderborn.de>
2023-08-26 15:44:37 +0200

    add vercel config

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from . import models
 from .database import engine
 from .routes import users, posts, auth, vote
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
