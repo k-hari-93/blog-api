8d0f7045a6d192c15322d320c0e41969d9d4a067
Hari Kizhedath <hari@mail.uni-paderborn.de>
2023-08-26 21:05:24 +0200

    minor changes

--- app/config.py
+++ app/config.py
@@ -1,17 +1,18 @@
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
     database_hostname: str
-    database_port: str 
-    database_password: str 
+    database_port: str
+    database_password: str
     database_name: str
     database_username: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
-    
+
     class Config:
-        env_file = ".env"
-        
-  
+        env_file = ".env.dev"
+
+
 settings = Settings()
